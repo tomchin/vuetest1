6670b8f314120ba943fed88df50864014239e9b1
gbqf83 <gbqf83@motorola.com>
2019-03-20 21:49:43 -0700

    add hellophone component

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,20 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <hellophone/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import hellophone from './components/hellophone.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    hellophone
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/hellophone.vue
@@ -0,0 +1,3 @@
+<template>
+      <h1> hello phone (vuetest1) </h1>
+</template>
