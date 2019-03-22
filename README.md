ea661c9d37f0af5cef6ab42e32c1f3aab27abad3
gbqf83 <gbqf83@motorola.com>
2019-03-22 12:01:39 -0700

    add MintUI and Capacitor

--- README.md
+++ README.md
@@ -1,5 +1,26 @@
 # vuetest1
 
+## Pointers
+
+``` bash
+# https://nuxtjs.org/guide/installation
+$ npx @vue/cli create vuetest1
+
+# http://mint-ui.github.io/docs/#/en2/quickstart
+$ npm install --save mint-ui
+
+# https://capacitor.ionicframework.com/docs/getting-started/
+$ npm install --save @capacitor/core @capacitor/cli
+$ npx cap init
+
+# https://capacitor.ionicframework.com/docs/basics/workflow
+$ npx cap add android
+$ npm run build
+$ npx cap open android
+$ npm run build
+$ npx cap copy
+```
+
 ## Project setup
 ```
 npm install
--- src/components/hellophone.vue
+++ src/components/hellophone.vue
@@ -1,3 +1,37 @@
 <template>
-      <h1> hello phone (vuetest1) </h1>
+  <div class="css-flex">
+    <mt-button type="primary" @click="handleClick1">Button 1</mt-button>
+    <mt-button plain @click="handleClick2">Button 2</mt-button>
+  </div>
 </template>
+
+<script>
+import Vue from 'vue';
+import MintUI from 'mint-ui';
+import 'mint-ui/lib/style.css'
+Vue.use(MintUI)
+
+export default {
+  data() {
+    return {};
+  },
+  methods: {
+    handleClick1: function() {
+      MintUI.Toast("Hello world!");
+    },
+    handleClick2: function() {
+      MintUI.MessageBox('MessageBox', "Hello world!");
+    }
+  }
+};
+</script>
+
+<style scoped>
+.css-flex {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+</style>
+
