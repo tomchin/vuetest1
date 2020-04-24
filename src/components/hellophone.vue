6e2549e62c3bafb639e01e2802e77e0f25c6cb3a
tomchin <tomchin.work@gmail.com>
2020-04-24 13:16:28 -0700

    updates

--- src/components/hellophone.vue
+++ src/components/hellophone.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="css-flex">
     <mt-button type="primary" @click="handleClick1">Button 1</mt-button>
+    <input v-model="in_msg" placeholder="edit me">
     <mt-button plain @click="handleClick2">Button 2</mt-button>
+    <textarea v-model="msg_out" rows="20" cols="30"></textarea>
   </div>
 </template>
 
@@ -15,17 +17,21 @@ import { Plugins } from "@capacitor/core";
 
 export default {
   data() {
-    return {};
+    return {
+      in_msg: "",
+      msg_out: "",
+    };
   },
   methods: {
     handleClick1: function() {
       MintUI.Toast("MintUI.Toast");
     },
     async handleClick2() {
+      this.msg_out += this.in_msg + "\n";
       await Plugins.Toast.show({
         text: "Native Toast"
       });
-    },
+    }
   }
 };
 </script>
